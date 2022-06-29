3154f2f27ac0a837f3363f6620b41ed9560f6288
yuchannn <yuchanbpp@gmail.com>
2022-06-29 14:46:55 +0900

    middleware

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // Allows the request to continue to the next middleware in line
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
