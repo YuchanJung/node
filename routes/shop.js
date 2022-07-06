40477bb05510ded3392d67a3eff19c83d1764775
yuchannn <yuchanbpp@gmail.com>
2022-07-06 22:14:03 +0900

    handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,12 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "shop",
+    path: "/",
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
