76eb50f6d66db75e5f6b7a749d0f075cfa04394b
yuchannn <yuchanbpp@gmail.com>
2022-07-09 16:26:37 +0900

    models

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -11,17 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
-    prods: products,
-    pageTitle: "shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    productCSS: true,
-    activeShop: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      productCSS: true,
+      activeShop: true,
+    });
   });
 };
---
+++ models/product.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    const filePath = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(filePath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(filePath, JSON.stringify(products), (err) =>
+        console.log(err)
+      );
+    });
+  }
+
+  static fetchAll(callback) {
+    const filePath = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(filePath, (err, fileContent) => {
+      if (err) callback();
+      callback(JSON.parse(fileContent));
+    });
+  }
+};
