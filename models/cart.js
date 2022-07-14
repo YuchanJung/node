cdf2eb4859a1fc0e4bb57b7d93dfe6d8d5ae2db9
yuchannn <yuchanbpp@gmail.com>
2022-07-14 21:06:03 +0900

    refactoring

--- models/cart.js
+++ models/cart.js
@@ -14,13 +14,12 @@ module.exports = class Cart {
       let cart = { products: [], totalPrice: 0 };
       if (!err) cart = JSON.parse(fileContent);
       // Analyze the cart => Find existing product
-      const existingProductIndex = cart.products.findIndex(
-        (value) => value.id === id
-      );
+      const targetProduct = cart.products.find((value) => value.id === id);
       // Add new product / increase quantity
-      if (cart.products[existingProductIndex]) {
+      if (targetProduct) {
         // checking with Index causes error..
-        cart.products[existingProductIndex].quantity += 1;
+        // => replace targetProduct Index with targetProduct. js call by reference?
+        targetProduct.quantity += 1;
       } else {
         const updatedProduct = { id, quantity: 1 };
         cart.products = [...cart.products, updatedProduct];
