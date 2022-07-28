a465d47fa3aff5ba8075ece37f80aa4314e219e5
yuchannn <yuchanbpp@gmail.com>
2022-07-28 15:30:55 +0900

    cart to orders & make order page

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,6 +45,7 @@ exports.getCart = (req, res, next) => {
       return cart.getProducts();
     })
     .then((products) => {
+      // console.log(products);
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
@@ -102,16 +103,52 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.getOrders = (req, res, next) => {
-  res.render("shop/order", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderItems = { quantity: product.cartItems.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    })
+    .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/order", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
+
+// exports.getCheckout = (req, res, next) => {
+//   res.render("shop/checkout", {
+//     path: "/checkout",
+//     pageTitle: "Checkout",
+//   });
+// };
--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,7 @@ const {
   getProduct,
   postCart,
   postCartDeleteProduct,
+  postOrder,
 } = require("../controllers/shop");
 
 const router = express.Router();
@@ -25,8 +26,10 @@ router.post("/cart", postCart);
 
 router.post("/cart-delete-product", postCartDeleteProduct);
 
+router.post("/create-order", postOrder);
+
 router.get("/orders", getOrders);
 
-router.get("/checkout", getCheckout);
+// router.get("/checkout", getCheckout);
 
 module.exports = router;
