fac0f9b1f0bebf948cc5212552f9d4d53d89a6e0
yuchannn <yuchanbpp@gmail.com>
2022-07-30 14:36:19 +0900

    mongodb initial setup

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const { Sequelize } = require("sequelize");
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://root:root@cluster0.chtnfx7.mongodb.net/?retryWrites=true&w=majority";
 
-const sequelize = new Sequelize("node-complete", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log("connected");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
