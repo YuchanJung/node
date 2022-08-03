d38d92e3fe7137274ba1144a857091d7e575f54c
yuchannn <yuchanbpp@gmail.com>
2022-08-03 15:52:47 +0900

    update order page - addOrder, getOrders

--- models/user.js
+++ models/user.js
@@ -66,6 +66,32 @@ class User {
       );
   }
 
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: this._id,
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] }; // optional??
+        return db
+          .collection("users")
+          .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db.collection("orders").find({ "user._id": this._id }).toArray();
+  }
+
   static findById(userId) {
     const db = getDb();
     return db.collection("users").findOne({ _id: new ObjectId(userId) });
--- routes/shop.js
+++ routes/shop.js
@@ -26,9 +26,9 @@ router.post("/cart", postCart);
 
 router.post("/cart-delete-product", postCartDeleteProduct);
 
-// router.post("/create-order", postOrder);
+router.post("/create-order", postOrder);
 
-// router.get("/orders", getOrders);
+router.get("/orders", getOrders);
 
 // router.get("/checkout", getCheckout);
 
