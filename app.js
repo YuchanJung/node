8bb40fe577f9613ed2e9a0bdb11552c0046a8e24
yuchannn <yuchanbpp@gmail.com>
2022-08-10 15:46:41 +0900

    login page setup for cookie prac

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const { get404 } = require("./controllers/error");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(get404);
 
