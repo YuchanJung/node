8b24fe303883a25401fc9b62893f835229ce310a
yuchannn <yuchanbpp@gmail.com>
2022-08-11 19:57:19 +0900

    practice cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  // const isAuthenticated = req.get("Cookie").split("=")[1];
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: false,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  // authentication
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
