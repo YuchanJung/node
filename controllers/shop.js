138118b47d4822af5a225cf07b8589ece54abb73
YuchanJung <yuchandev@gmail.com>
2022-10-29 23:21:20 +0900

    pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,13 +6,31 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/product-list", {
         products,
-        pageTitle: "All Products",
+        pageTitle: "Products",
         path: "/products",
+        currentPage: page,
+        hasPreviousPage: page > 1,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        previousPage: page - 1,
+        nextPage: page + 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
@@ -40,12 +58,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/index", {
         products,
         pageTitle: "Shop",
         path: "/",
+        currentPage: page,
+        hasPreviousPage: page > 1,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        previousPage: page - 1,
+        nextPage: page + 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
